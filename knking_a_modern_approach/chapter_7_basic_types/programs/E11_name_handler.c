674b6c441f37a33728e16d621e34d7366dc5e9ae
andr-be <benandrew65@gmail.com>
2023-07-15 13:29:04 +0100

    Finished C7_E11

---
+++ knking_a_modern_approach/chapter_7_basic_types/programs/E11_name_handler.c
@@ -0,0 +1,23 @@
+// NAME HANDLER
+// andr-be 07/2023
+/*
+    Reformats an entered name to the old `Surname -> F Initial` standard
+    e.g. Lloyd Fosdick -> Fosdick, L.
+*/
+
+#include <stdio.h>
+#define STR_LEN 20
+
+int main(void)
+{
+    // I wish I knew how better to initialise strings than as arrays!
+    char first[STR_LEN], last[STR_LEN];
+    printf("Enter a first and last name: ");
+
+    // also would be nice if I could set the scanf field with STR_LEN
+    scanf("%19s %19s", first, last);
+
+    // prints the last name and the first letter of the first name
+    printf("%s, %c.\n", last, first[0]);
+    return 0;
+}
